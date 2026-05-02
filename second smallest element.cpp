25abda77d8012ff6d135fe978c6714cde3f60948
Himanshu yadav <murfeyjack01@gmail.com>
2026-05-02 23:06:37 +0530

    Add functionality to find second smallest element
    Implement logic to find the second smallest element in an array.

--- second smallest element.cpp
+++ second smallest element.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <climits>
+using namespace std;
 
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    int smallest = arr[0];
+    int second = INT_MAX;
+
+    for(int i = 1; i < n; i++){
+        if(arr[i] < smallest){
+            second = smallest;
+            smallest = arr[i];
+        }
+        else if(arr[i] > smallest && arr[i] < second){
+            second = arr[i];
+        }
+    }
+
+    if(second == INT_MAX)
+        cout << "No second smallest";
+    else
+        cout << second;
+}
